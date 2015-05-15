f2b799c4afe2852c0c4e0701bdc0200a82e3db21
Mileta Cekovic <mileta.cekovic@igt.com>
2015-05-15 17:39:03 +0200

    Optimization

--- sf-orm/src/main/java/org/strangeforest/orm/EntityCache.java
+++ sf-orm/src/main/java/org/strangeforest/orm/EntityCache.java
@@ -8,21 +8,18 @@ import org.strangeforest.concurrent.*;
 final class EntityCache<I, E extends DomainEntity<I, E>> {
 
 	private final Map<I, E> l1;
-	private final Map<I, E> changed;
-	private final Set<I> deleted;
-	private final Set<I> locked;
 	private final LockableCache<I, E> l2;
 	private final boolean useCache;
 	private final boolean inTx;
+	private Map<I, E> changed;
+	private Set<I> deleted;
+	private Set<I> locked;
 
 	public EntityCache(LockableCache<I, E> l2, boolean useCache, boolean inTx) {
+		l1 = new HashMap<>();
+		this.l2 = l2;
 		this.useCache = useCache;
 		this.inTx = inTx;
-		l1 = new HashMap<>();
-		changed = new HashMap<>();
-		deleted = new HashSet<>();
-		locked = new HashSet<>();
-		this.l2 = l2;
 	}
 
 	public E get(I id) {
@@ -68,7 +65,7 @@ final class EntityCache<I, E extends DomainEntity<I, E>> {
 		l1.put(id, entity);
 		if (useCache) {
 			if (inTx)
-				changed.put(id, entity);
+				changed().put(id, entity);
 			else
 				l2.put(id, entity.deepClone());
 		}
@@ -79,9 +76,9 @@ final class EntityCache<I, E extends DomainEntity<I, E>> {
 		l1.put(id, entity);
 		if (useCache) {
 			if (inTx) {
-				changed.put(id, entity);
+				changed().put(id, entity);
 				l2.lock(id);
-				locked.add(id);
+				locked().add(id);
 			}
 			else
 				l2.lockedPut(id, entity.deepClone());
@@ -92,7 +89,7 @@ final class EntityCache<I, E extends DomainEntity<I, E>> {
 		l1.remove(id);
 		if (useCache) {
 			if (inTx)
-				deleted.add(id);
+				deleted().add(id);
 			else
 				l2.remove(id);
 		}
@@ -105,16 +102,15 @@ final class EntityCache<I, E extends DomainEntity<I, E>> {
 	}
 
 	public void lock(I id) {
-		if (useCache) {
+		if (useCache)
 			l2.lock(id);
-			if (inTx)
-				locked.add(id);
-		}
 	}
 
 	public void unlock(I id) {
 		if (useCache) {
-			if (!inTx)
+			if (inTx)
+				locked().add(id);
+			else
 				l2.unlock(id);
 		}
 	}
@@ -129,19 +125,23 @@ final class EntityCache<I, E extends DomainEntity<I, E>> {
 		if (!useCache)
 			return;
 		try {
-			for (Map.Entry<I, E> entry : changed.entrySet()) {
-				I id = entry.getKey();
-				E entity = entry.getValue();
-				if (locked.contains(id))
-					l2.put(id, entity.deepClone());
-				else
-					l2.lockedPut(id, entity.deepClone());
+			if (changed != null) {
+				for (Map.Entry<I, E> entry : changed.entrySet()) {
+					I id = entry.getKey();
+					E entity = entry.getValue();
+					if (locked.contains(id))
+						l2.put(id, entity.deepClone());
+					else
+						l2.lockedPut(id, entity.deepClone());
+				}
 			}
-			for (I id : deleted) {
-				if (locked.contains(id))
-					l2.remove(id);
-				else
-					l2.lockedRemove(id);
+			if (deleted != null) {
+				for (I id : deleted) {
+					if (locked.contains(id))
+						l2.remove(id);
+					else
+						l2.lockedRemove(id);
+				}
 			}
 		}
 		finally {
@@ -156,7 +156,27 @@ final class EntityCache<I, E extends DomainEntity<I, E>> {
 	}
 
 	private void unlock() {
-		for (I id : locked)
-			l2.unlock(id);
+		if (locked != null) {
+			for (I id : locked)
+				l2.unlock(id);
+		}
+	}
+
+	private Map<I, E> changed() {
+		if (changed == null)
+			changed = new HashMap<>();
+		return changed;
+	}
+
+	private Set<I> deleted() {
+		if (deleted == null)
+			deleted = new HashSet<>();
+		return deleted;
+	}
+
+	private Set<I> locked() {
+		if (locked == null)
+			locked = new HashSet<>();
+		return locked;
 	}
 }
